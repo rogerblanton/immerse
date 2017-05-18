f44f4baca478af65de36bcf5d0866f856e6eb338
Roger Blanton <blanton@adobe.com>
2017-05-18 01:29:58 -0400

    removing libs folder from codebase

--- content/src/main/content/jcr_root/libs/foundation/components/form/actions/store/clientlibs/store.js
+++
@@ -1,7 +0,0 @@
-(function (document, $) {
-    $(document).on("foundation-contentloaded", function (e) {
-        $('.js-store-view-data').on('click', function (e) {
-            window.open(Granite.HTTP.externalize($(".js-store-path").val() + ".json"), "FormReport");
-        });
-    });
-})(document, Granite.$);
